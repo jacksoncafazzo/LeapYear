6a48fb0a8284e8feb105387fb2a8accb1fae67ab
Jackson Cafazzo and Ryan Brown <doctorjuno@gmail.com>
2016-01-19 09:55:24 -0800

    Add NaN test to logic.

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,21 @@
 var leapYear = function(year) {
-  return ((year % 4 === 0) && (year % 100 !== 0)) || (year % 400 === 0);
+  return ((year % 4 === 0) && (year % 100 !== 0) && (year > 0)) || (year % 400 === 0) ;
 };
 
 $(document).ready(function() {
   $("form#leap-year").submit(function(event) {
     var year = parseInt($("input#year").val());
     var result = leapYear(year);
-
     $(".year").text(year);
 
-    if (!result) {                 // same as writing if (result === false)
-      $(".not").text("not");
-    } else {
-      $(".not").text("");
-    }
-
-    $("#result").show();
-    event.preventDefault();
+    if (year === "NaN") {
+      $("#NaN").show();
+    } else if (!result) {                 // same as writing if (result === false)
+        $(".not").text("not");
+      } else {
+        $(".not").text("");
+      }
+      $("#result").show();
+      event.preventDefault();
   });
 });
--- spec/specs.js
+++ spec/specs.js
@@ -14,4 +14,12 @@ describe('leapYear', function() {
   it("is true for years divisible by 400", function() {
   expect(leapYear(2000)).to.equal(true);
   });
+
+  it("is false for negative number", function() {
+    expect(leapYear(-831)).to.equal(false);
+  });
+
+  it("is false for strings", function() {
+    expect(leapYear("word")).to.equal(false);
+  });
 });
